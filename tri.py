8beff0b0123b7aa584ed5e2d46aab7b9277d6a87
Leo Cheung <97723989+LeoCheung0804@users.noreply.github.com>
2022-12-10 06:55:54 +0800

    Add files via upload

--- tri.py
+++ tri.py
@@ -1,10 +1,19 @@
-import math
+import numpy as np
 
 def tand(x):
-    return math.tan(x * math.pi / 180)
+    return np.tan(x * np.pi / 180)
 
 def sind(x):
-    return math.sin(x * math.pi / 180)
+    return np.sin(x * np.pi / 180)
 
 def cosd(x):
-    return math.cos(x * math.pi / 180)
+    return np.cos(x * np.pi / 180)
+
+def atand(x):
+    return np.arctan(x) * 180 / np.pi
+
+def asind(x):
+    return np.arcsin(x) * 180 / np.pi
+
+def acosd(x):
+    return np.arccos(x) * 180 / np.pi
